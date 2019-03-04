69c0cbb4c1448c56e7131c158010e67403283224
berkehanozen <brkhnzn@gmail.com>
2019-03-05 02:43:00 +0300

    Completely covered the Util class by expanding the amount of tests conducted.

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -6,11 +6,26 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
-  Util c;
+    Util c;
 
-  @Before
-  public void setUp() { c = new Util(); }
+    @Before
+    public void setUp() { c = new Util(); }
 
-  @Test
-  public void example() { assertTrue(true); }
+    @Test
+    public void example() { assertTrue(true); }
+
+    @Test
+    public void testLength1() { assertFalse(c.compute(2)); }
+
+    @Test
+    public void testLengthEven() { assertFalse(c.compute(1,2,3,4)); }
+
+    @Test
+    public void testModTrue() { assertTrue(c.compute(1,2,3,4,5)); }
+
+    @Test
+    public void testModFalse() { assertFalse(c.compute(5,5,6)); }
+
+    @Test(expected = RuntimeException.class)
+    public void testException() { c.compute(1,0,2); }
 }
