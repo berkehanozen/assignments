c1d09db49c5c7c8f33a3ec1825ecfcf0fa3931a7
berkehanozen <brkhnzn@gmail.com>
2019-03-06 19:11:10 +0300

    By adding additional tests to TestCompute, we have achieved complete code coverage on Compute class. This means that together with the comletely covered Util class, we have achieved comlete coverage on all codes.

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestCompute {
   Compute c;
@@ -14,4 +15,41 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void testSize0() {
+    MessageQueue mq = mock(MessageQueue.class);
+    Mockito.when(mq.size()).thenReturn(0);
+    c = new Compute(mq);
+    assertEquals(c.countNumberOfOccurrences("Anything"),-1);
+  }
+
+  @Test
+  public void testNotContains() {
+      MessageQueue mq = mock(MessageQueue.class);
+      Mockito.when(mq.contains(anyString())).thenReturn(false);
+      Mockito.when(mq.size()).thenReturn(1);
+      c = new Compute(mq);
+      assertEquals(c.countNumberOfOccurrences("Anything"),0);
+  }
+
+    @Test
+    public void testCounterNot0() {
+        MessageQueue mq = mock(MessageQueue.class);
+        Mockito.when(mq.contains(anyString())).thenReturn(true);
+        Mockito.when(mq.size()).thenReturn(5);
+        Mockito.when(mq.getAt(anyInt())).thenReturn("Anything");
+        c = new Compute(mq);
+        assertEquals(c.countNumberOfOccurrences("Anything"),5);
+    }
+
+    @Test
+    public void testCounter0() {
+        MessageQueue mq = mock(MessageQueue.class);
+        Mockito.when(mq.contains(anyString())).thenReturn(true);
+        Mockito.when(mq.size()).thenReturn(5);
+        Mockito.when(mq.getAt(anyInt())).thenReturn(" ");
+        c = new Compute(mq);
+        assertEquals(c.countNumberOfOccurrences("Anything"),0);
+    }
 }
